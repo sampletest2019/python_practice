cc43e72b7042ef4f5756b59a2302d1d7781b5846
Yury K <yuriyfromrussia@hotmail.com>
2021-06-10 21:20:19 -0400

    minor refactoring.

--- homework_1.py
+++ homework_1.py
@@ -1,42 +1,41 @@
-from selenium import webdriver
-import time
+# create 3 int variables. Division result is always a float. Use // to avoid
+int_1 = 750
+int_2 = int_1 + 60
+int_3 = int_2 // 2
 
-line_1 = 750
-line_2 = line_1 + 60
-line_3 = line_2 // 2
+# print int variables values
+print("line_1=", int_1)
+print("line-2=", int_2)
+print("line_3=", int_3)
 
-print("line_1=", line_1)
-print("line-2=", line_2)
-print("line_3=", line_3)
-
+# create 3 float variables
 float_1 = 14.2
 float_2 = 75.12
 float_3 = 11.31
 
+# print 3 float variables values
 print(float_1)
 print(float_2)
 print(float_3)
 
-# The first variable: should equal to the sum of the second and the third
-line_1 = line_2 + line_3
-print("sum=", line_1)
-line_2 -= 100
-print("second variable", line_2)
-line_3 += 1
-print("third variable", line_3)
+# The first int variable: should equal to the sum of the second and the third
+int_1 = int_2 + int_3
+print("sum=", int_1)
 
+# subtract 100 from the second int variable
+int_2 -= 100
+print("second variable", int_2)
+
+# increment (add 1) third int variable
+int_3 += 1
+print("third variable", int_3)
+
+# make first float negative
 print(-float_1)
 
-# negate the value
-print("negate_the_value=", line_3 - line_1)
-
-# The second variable: should be reduced by 100
-print("reduced_by_100=", line_2 - 100)
-# should equal to the twice the value of the third variable, minus the value of the first
+# second float should be equal to the twice the value of the third variable, minus the value of the first
 float_2 = float_3 * 2 - float_1
 print("line_2=", "(line_2*2-line_1=)", float_2)
 
-# The third variable: increment its value (means “add 1”)
-print("increment_its_value=", line_3 + 1)
-# divide its value by 31
+# divide third float value by 3
 print("line_3/3=", float_3 / 3)
