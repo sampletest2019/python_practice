39fc3c9a89101e66b692d300095fbda72164b5c2
Yury K <yuriyfromrussia@hotmail.com>
2021-06-17 19:18:00 -0400

    test_schedule_page.py method find_element_enter_text created.

--- selenium/test_schedule_page.py
+++ selenium/test_schedule_page.py
@@ -18,6 +18,14 @@ customer = {
 
 # create an instance of Chrome
 browser = webdriver.Chrome(ChromeDriverManager().install())
+
+# create function to find an element by name which we will provide
+# and enter the text into this text field which we will provide
+def find_element_enter_text(name, text):
+    browser.find_element_by_name(name).send_keys(text)
+
+
+
 # navigate to home pro base url (home page) see above
 browser.get(base_url)
 # validate that current url is matching with our expected url (home page url)
@@ -32,13 +40,19 @@ jobtype_dropdown = Select(browser.find_element_by_name("job_type"))
 # now we will select job type from the dropdown by index (0, 1, 2 etc)
 jobtype_dropdown.select_by_index(customer["jop type index"])
 
-browser.find_element_by_name("first_name").send_keys(customer["first name"])
-browser.find_element_by_name("last_name").send_keys(customer["last name"])
-browser.find_element_by_name("phone").send_keys(customer["phone"])
-browser.find_element_by_name("email").send_keys(customer["email"])
+# we used our newly created function to find different elements
+# by name and enter different text
+find_element_enter_text("first_name", customer["first name"])
+find_element_enter_text("last_name", customer["last name"])
+find_element_enter_text("phone", customer["phone"])
+find_element_enter_text("email", customer["email"])
+# browser.find_element_by_name("first_name").send_keys(customer["first name"])
+# browser.find_element_by_name("last_name").send_keys(customer["last name"])
+# browser.find_element_by_name("phone").send_keys(customer["phone"])
+# browser.find_element_by_name("email").send_keys(customer["email"])
 
 # find a button by xpath and click on it
-browser.find_element_by_xpath("//input[@value='Schedule My consultation']").click()
-# validate that user landed on the confirmation page by checking that url matches expected url (confirmation page)
-assert browser.current_url == confirmation_page
+# browser.find_element_by_xpath("//input[@value='Schedule My consultation']").click()
+# # validate that user landed on the confirmation page by checking that url matches expected url (confirmation page)
+# assert browser.current_url == confirmation_page
 
