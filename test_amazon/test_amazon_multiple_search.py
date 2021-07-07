2c770d1547b28d6718cb274982c188967dab3a45
Yury K <yuriyfromrussia@hotmail.com>
2021-07-06 20:51:43 -0400

    test_amazon_multiple_search.py added.

---
+++ test_amazon/test_amazon_multiple_search.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.mark.parametrize("name", [
+    "macbook pro",
+    "microsoft surface",
+    "razer gaming laptop"
+])
+@pytest.mark.regressiontest
+def test_validate_search(browser, item):
+    browser.get("https://www.amazon.com")
+    assert browser.title == "Amazon.com. Spend less. Smile more."
+    browser.find_element_by_id("twotabsearchtextbox").send_keys(item + Keys.ENTER)
